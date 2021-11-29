05395b6e1fbb1aebabac2affbe0788c37dccbd33
ofego favour <ofegofavour@gmail.com>
2021-11-29 09:00:11 +0100

    first commit

---
+++ accordion/app.js
@@ -0,0 +1,7 @@
+let accordions = document.querySelectorAll('.accordion-container .accordion');
+accordions.forEach(acco =>{
+    acco.onclick = () =>{
+        accordions.forEach(subAcco => {subAcco.classList.remove('active')});
+        acco.classList.add('active');
+    }
+})
